5871d83b6c1ec7ea966f77372d3369914a79f20b
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-23 16:09:43 -0700

    seed github_users table and posts table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS github_users;
+
+CREATE TABLE github_users (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username TEXT NOT NULL,
+  email TEXT,
+  avatar TEXT
+);
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  post VARCHAR (255) NOT NULL,
+  username TEXT NOT NULL,
+  avatar TEXT
+);
