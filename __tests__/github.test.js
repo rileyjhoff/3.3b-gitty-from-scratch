47de9c706c309c9ce4c443b69fe48615c313c073
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-24 10:24:59 -0700

    write test for GET /api/v1/github/login. add GET /api/v1/github/login and GET /api./v1/github/callback routes to github controller

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -3,13 +3,21 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+jest.mock('../lib/services/github.js');
+
+describe('github routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('GET /api/v1/github/login route should redirect to the oauth page upon login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
+
   afterAll(() => {
     pool.end();
   });
